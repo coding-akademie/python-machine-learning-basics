2e0148993f02a5a2cf379a3b2ca3d86b367fc5fc
Matthias Hölzl <tc@xantira.com>
2020-09-03 17:13:17 +0200

    End of course, fix simple dice

--- Examples/HttpServer/my_http_server.py
+++ Examples/HttpServer/my_http_server.py
@@ -1,2 +1,11 @@
 # Code a HTTP Server here...
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+
+def main():
+    httpd = HTTPServer(("", 8000), SimpleHTTPRequestHandler)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Examples/HttpServer/test.py
@@ -0,0 +1,5 @@
+import my_http_server
+
+
+def test_compute_sth():
+    my_http_server.compute_sth()
